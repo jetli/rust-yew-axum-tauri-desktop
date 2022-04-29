e25242d7fd861a2cb1f9a635d7cd1bcb28b5ea57
Jet Li <jing.i.qin@icloud.com>
2022-04-29 23:52:00 +0800

    setup websocket for backend/server

--- crates/backend/src/lib.rs
+++ crates/backend/src/lib.rs
@@ -1,18 +1,25 @@
 use std::net::SocketAddr;
 
-use axum::{http::Method, response::IntoResponse, routing::get, Router};
+use axum::{
+    extract::ws::{Message, WebSocket, WebSocketUpgrade},
+    http::Method,
+    response::IntoResponse,
+    routing::get,
+    Router,
+};
 use tower_http::cors::{Any, CorsLayer};
 
 pub async fn app(port: u16) {
-    let app = Router::new().route("/", get(handler)).layer(
-        CorsLayer::new().allow_origin(Any).allow_methods(vec![
+    let app = Router::new()
+        .route("/ws", get(ws_handler))
+        .route("/", get(handler))
+        .layer(CorsLayer::new().allow_origin(Any).allow_methods(vec![
             Method::GET,
             Method::POST,
             Method::PUT,
             Method::PATCH,
             Method::DELETE,
-        ]),
-    );
+        ]));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], port));
     println!("Backend is listening on http://{}", addr);
@@ -24,4 +31,35 @@ pub async fn app(port: u16) {
 
 async fn handler() -> impl IntoResponse {
     "Hello, from backend!"
+}
+
+async fn ws_handler(ws: WebSocketUpgrade) -> impl IntoResponse {
+    ws.on_upgrade(handle_socket)
+}
+
+async fn handle_socket(mut socket: WebSocket) {
+    loop {
+        if let Some(msg) = socket.recv().await {
+            if let Ok(msg) = msg {
+                match msg {
+                    Message::Text(t) => {
+                        // Echo
+                        if socket
+                            .send(Message::Text(format!("Echo from backend: {}", t)))
+                            .await
+                            .is_err()
+                        {
+                            return;
+                        }
+                    }
+                    Message::Close(_) => {
+                        return;
+                    }
+                    _ => {}
+                }
+            } else {
+                return;
+            }
+        }
+    }
 }
--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -1,15 +1,24 @@
 use std::net::SocketAddr;
 
-use axum::{http::Method, response::IntoResponse, routing::get, Router};
+use axum::{
+    extract::ws::{Message, WebSocket, WebSocketUpgrade},
+    http::Method,
+    response::IntoResponse,
+    routing::get,
+    Router,
+};
 use tower_http::cors::{Any, CorsLayer};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(handler)).layer(
-        CorsLayer::new()
-            .allow_origin(Any)
-            .allow_methods(vec![Method::GET, Method::POST]),
-    );
+    let app = Router::new()
+        .route("/ws", get(ws_handler))
+        .route("/", get(handler))
+        .layer(
+            CorsLayer::new()
+                .allow_origin(Any)
+                .allow_methods(vec![Method::GET, Method::POST]),
+        );
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3001));
     println!("listening on {}", addr);
@@ -21,4 +30,35 @@ async fn main() {
 
 async fn handler() -> impl IntoResponse {
     "Hello, from server!"
+}
+
+async fn ws_handler(ws: WebSocketUpgrade) -> impl IntoResponse {
+    ws.on_upgrade(handle_socket)
+}
+
+async fn handle_socket(mut socket: WebSocket) {
+    loop {
+        if let Some(msg) = socket.recv().await {
+            if let Ok(msg) = msg {
+                match msg {
+                    Message::Text(t) => {
+                        // Echo
+                        if socket
+                            .send(Message::Text(format!("Echo from backend: {}", t)))
+                            .await
+                            .is_err()
+                        {
+                            return;
+                        }
+                    }
+                    Message::Close(_) => {
+                        return;
+                    }
+                    _ => {}
+                }
+            } else {
+                return;
+            }
+        }
+    }
 }
