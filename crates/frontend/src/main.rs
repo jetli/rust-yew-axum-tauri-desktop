c8ed542f033666a5a477797bc7d4449c00c3e995
Jet Li <jing.i.qin@icloud.com>
2022-05-02 09:31:20 +0800

    setup websocket for frontend

--- crates/frontend/src/main.rs
+++ crates/frontend/src/main.rs
@@ -1,6 +1,9 @@
 use wasm_bindgen::prelude::*;
 use yew::prelude::*;
-use yew_hooks::{use_async, use_async_with_options, UseAsyncOptions};
+use yew_hooks::{
+    use_async, use_async_with_options, use_list, use_web_socket_with_options, UseAsyncOptions,
+    UseWebSocketOptions, UseWebSocketReadyState,
+};
 
 // Use `wee_alloc` as the global allocator.
 #[global_allocator]
@@ -8,32 +11,36 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[function_component(App)]
 fn app() -> Html {
-    // Get backend url with a port automatically.
-    let backend = use_async_with_options(
+    // Get backend port automatically from tauri command.
+    let port = use_async_with_options(
         async move {
             match get_port().await {
-                Ok(p) => Ok(format!("http://localhost:{}/", p.as_string().unwrap())),
+                Ok(p) => Ok(p.as_string().unwrap()),
                 Err(e) => Err(format!("Error: {:?}", e)),
             }
         },
         UseAsyncOptions::enable_auto(),
     );
 
-    let state = use_async(async move {
-        match &backend.data {
-            Some(url) => {
-                let response = reqwest::get(url).await;
-                match response {
-                    Ok(data) => match data.text().await {
-                        Ok(body) => Ok(body),
-                        Err(_) => Err("Backend body Error".to_owned()),
-                    },
-                    Err(_) => Err("Backend request Error".to_owned()),
+    // Fetch data from backend.
+    let state = {
+        let port = port.clone();
+        use_async(async move {
+            match &port.data {
+                Some(port) => {
+                    let response = reqwest::get(format!("http://localhost:{}/", port)).await;
+                    match response {
+                        Ok(data) => match data.text().await {
+                            Ok(body) => Ok(body),
+                            Err(_) => Err("Backend body Error".to_owned()),
+                        },
+                        Err(_) => Err("Backend request Error".to_owned()),
+                    }
                 }
+                _ => Err("Backend is unavailable".to_owned()),
             }
-            _ => Err("Backend is unavailable".to_owned()),
-        }
-    });
+        })
+    };
 
     let onclick = {
         let state = state.clone();
@@ -42,6 +49,7 @@ fn app() -> Html {
         })
     };
 
+    // Fetch data from server.
     let state_server = use_async(async move {
         let response = reqwest::get("http://localhost:3001/").await;
         match response {
@@ -60,17 +68,51 @@ fn app() -> Html {
         })
     };
 
+    let history = use_list(vec![]);
+
+    // Manually connect to websocket with custom options.
+    let ws = {
+        let history = history.clone();
+        let port = port.data.clone().unwrap_or_default();
+        use_web_socket_with_options(
+            format!("ws://localhost:{}/ws", port),
+            UseWebSocketOptions {
+                // Receive message by callback `onmessage`.
+                onmessage: Some(Box::new(move |message| {
+                    history.push(format!("ws [recv]: {}", message));
+                })),
+                manual: Some(true),
+                ..Default::default()
+            },
+        )
+    };
+    let onclick2 = {
+        let ws = ws.clone();
+        let history = history.clone();
+        Callback::from(move |_| {
+            let message = "Hello, backend!".to_string();
+            ws.send(message.clone());
+            history.push(format!("ws [send]: {}", message));
+        })
+    };
+    let onopen = {
+        let ws = ws.clone();
+        Callback::from(move |_| {
+            ws.open();
+        })
+    };
+
     html! {
         <>
-            <button {onclick}>{ "Load backend api" }</button>
-            <button onclick={onclickserver}>{ "Load server api" }</button>
+            <p>
+                <button {onclick}>{ "Load backend api" }</button>
+                <button onclick={onclickserver}>{ "Load server api" }</button>
+            </p>
             {
                 if let Some(response) = &state.data {
                     html! {
-                        <>
-                            <p>{ "From backend: " }<b>{ response }</b></p>
-                        </>
-                        }
+                        <p>{ "From backend: " }<b>{ response }</b></p>
+                    }
                 } else {
                     html! {}
                 }
@@ -78,13 +120,22 @@ fn app() -> Html {
             {
                 if let Some(response) = &state_server.data {
                     html! {
-                        <>
-                            <p>{ "From server: " }<b>{ response }</b></p>
-                        </>
-                        }
+                        <p>{ "From server: " }<b>{ response }</b></p>
+                    }
                 } else {
                     html! {}
                 }
+            }
+            <p>
+                <button onclick={onopen} disabled={*ws.ready_state != UseWebSocketReadyState::Closed}>{ "Connect to backend websocket" }</button>
+                <button onclick={onclick2} disabled={*ws.ready_state != UseWebSocketReadyState::Open}>{ "Send to backend websocket" }</button>
+            </p>
+            {
+                for history.current().iter().map(|message| {
+                    html! {
+                        <p>{ message }</p>
+                    }
+                })
             }
         </>
     }
